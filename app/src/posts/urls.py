22465ea1c3fd574591255c1456c2ec68195f37f9
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-05-28 13:20:22 -0300

    templates

--- app/src/posts/urls.py
+++ app/src/posts/urls.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
-from . import views
+from .views import (
+    post_list,
+    post_create,
+    post_list,
+    post_update,
+    post_delete
+)
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    path('create/', views.post_create)
+    url(r'^$', post_list),
+    path('create/', post_create),
+    url(r'^detail/$', post_list),
+    path('update/', post_update),
+    path('delete/', post_delete),
 ]
 
 # url(r'^anything$') is the same path(anything)
--- app/src/posts/views.py
+++ app/src/posts/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>post_create</h1>")
+    # return HttpResponse("<h1>"+os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"</h1>")
+    return render(request, "index.html", {})
 
 def post_detail(request):
-    return HttpResponse("<h1>Hpost_detailello</h1>")
+    return HttpResponse("<h1>post_detail</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>post_list</h1>")
+    return render(request, "index.html", {})
 
 def post_update(request):
     return HttpResponse("<h1>post_update</h1>")
