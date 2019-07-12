dc54a97dc183d8fb28dd62c50fbf4b114899b2f4
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-12 18:10:21 -0300

    creating createview

--- app/src/demands/urls.py
+++ app/src/demands/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from .views import (
     demands_index,
+    demands_create
 )
 
 urlpatterns = [
     url(r'^$', demands_index),
+    path('index/', demands_index),
+    path('create/', demands_create)
 ]
 
 # url(r'^anything$') is the same path(anything)
--- app/src/demands/views.py
+++ app/src/demands/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def demands_index(request):
     context = {
-        "title": "Demands"
+        "page": "Demands",
+        "subpage": "Retrieving all"
     }
     return render(request, "./demands/index.html", context)
+
+def demands_create(request):
+    context = {
+        "page": "Demands",
+        "subpage": "Creating"
+    }
+    return render(request, "./demands/create.html", context)
